7da1de481f71b0251f45726772b8e70e9ac47ca6
TNeitzel <qtc_de@outlook.com>
2020-10-25 07:34:20 +0100

    Update logger class

--- src/de/qtc/rmg/io/Logger.java
+++ src/de/qtc/rmg/io/Logger.java
@@ -2,98 +2,184 @@ package de.qtc.rmg.io;
 
 public class Logger {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
 
     public static int indent = 0;
     public static boolean verbose = true;
 
-    public static void print(String msg) {
+    private static String blue(String msg)
+    {
+        return ANSI_BLUE + msg + ANSI_RESET;
+    }
+
+    private static String yellow(String msg)
+    {
+        return ANSI_YELLOW + msg + ANSI_YELLOW;
+    }
+
+    private static String prefix()
+    {
+        return "[+]" + Logger.getIndent();
+    }
+
+    private static String eprefix()
+    {
+        return "[-]" + Logger.getIndent();
+    }
+
+    private static void log(String msg)
+    {
+        log(msg, false);
+    }
+
+    private static void log(String msg, boolean newline)
+    {
         if( Logger.verbose ) {
-            System.out.print("[+]" + Logger.getIndent() + msg);
+
+            if( newline ) 
+                System.out.println(msg);
+            else
+                System.out.print(msg);
         }
     }
 
-    public static void eprint(String msg) {
-        System.err.print("[-]" + Logger.getIndent() + msg);
+    private static void elog(String msg)
+    {
+        elog(msg, false);
     }
 
-    public static void println(String msg) {
-        if( Logger.verbose ) {
-            System.out.println("[+]" + Logger.getIndent() + msg);
-        }
-    }
-
-    public static void printlnPlain(String msg) {
-        if( Logger.verbose ) {
+    private static void elog(String msg, boolean newline)
+    {
+        if( newline )
             System.out.println(msg);
-        }
+        else
+            System.out.print(msg);
     }
 
-    public static void eprintln(String msg) {
-        System.err.println("[-]" + Logger.getIndent() + msg);
+    public static void print(String msg)
+    {
+        log(prefix() + msg, false);
     }
 
-    public static void eprintlnPlain(String msg) {
-        System.err.println(msg);
+    public static void println(String msg)
+    {
+        log(prefix() + msg, true);
     }
 
-    public static void println_bl(String msg) {
-        if( Logger.verbose ) {
-            System.out.println("[+]" + Logger.getIndent() + ANSI_BLUE + msg + ANSI_RESET);
-        }
+    public static void printlnPlain(String msg)
+    {
+        log(msg, true);
     }
 
-    public static void eprintln_bl(String msg) {
-        System.err.println("[-]" + Logger.getIndent() + ANSI_BLUE + msg + ANSI_RESET);
+    public static void eprint(String msg)
+    {
+        elog(eprefix() + msg, false);
     }
 
-    public static void println_ye(String msg) {
-        if( Logger.verbose ) {
-            System.out.println("[+]" + Logger.getIndent() + ANSI_YELLOW + msg + ANSI_RESET);
-        }
+    public static void eprintln(String msg)
+    {
+        elog(eprefix() + msg, true);
     }
 
-    public static void eprintln_ye(String msg) {
-        System.err.println("[-]" + Logger.getIndent() + ANSI_YELLOW + msg + ANSI_RESET);
+    public static void eprintlnPlain(String msg)
+    {
+        elog(msg, true);
     }
 
-    public static void printlnPlain_bl(String msg) {
-        if( Logger.verbose ) {
-            System.out.println(ANSI_BLUE + msg + ANSI_RESET);
-        }
+    public static void printlnBlue(String msg)
+    {
+        log(prefix() + blue(msg), true);
     }
 
-    public static void eprintlnPlain_bl(String msg) {
-        System.err.println(ANSI_BLUE + msg + ANSI_RESET);
+    public static void eprintlnBlue(String msg)
+    {
+        elog(prefix() + blue(msg), true);
     }
 
-    public static void printlnPlain_ye(String msg) {
-        if( Logger.verbose ) {
-            System.out.println(ANSI_YELLOW + msg + ANSI_RESET);
-        }
+    public static void printlnYellow(String msg)
+    {
+        log(prefix() + yellow(msg), true);
     }
 
-    public static void eprintlnPlain_ye(String msg) {
-        System.err.println(ANSI_YELLOW + msg + ANSI_RESET);
+    public static void eprintlnYellow(String msg)
+    {
+        elog(prefix() + yellow(msg), true);
     }
 
-    public static void increaseIndent() {
+    public static void printlnPlainBlue(String msg)
+    {
+        log(blue(msg), true);
+    }
+
+    public static void eprintlnPlainBlue(String msg)
+    {
+        elog(blue(msg), true);
+    }
+
+    public static void printlnPlainYellow(String msg)
+    {
+        log(yellow(msg), true);
+    }
+
+    public static void eprintlnPlainYellow(String msg)
+    {
+        elog(yellow(msg), true);
+    }
+
+    public static void printlnMixedBlue(String first, String second)
+    {
+        log(prefix() + first + " " + blue(second));
+    }
+
+    public static void printlnMixedBlue(String first, String second, String third)
+    {
+        log(prefix() + first + " " + blue(second) + " " + third);
+    }
+
+    public static void printlnMixedYellow(String first, String second)
+    {
+        log(prefix() + first + " " + yellow(second));
+    }
+
+    public static void printlnMixedYellow(String first, String second, String third)
+    {
+        log(prefix() + first + " " + yellow(second) + " " + third);
+    }
+
+    public static void eprintlnMixedBlue(String first, String second)
+    {
+        elog(prefix() + first + " " + blue(second));
+    }
+
+    public static void eprintlnMixedBlue(String first, String second, String third)
+    {
+        elog(prefix() + first + " " + blue(second) + " " + third);
+    }
+
+    public static void eprintlnMixedYellow(String first, String second)
+    {
+        elog(prefix() + first + " " + yellow(second));
+    }
+
+    public static void eprintlnMixedYellow(String first, String second, String third)
+    {
+        elog(prefix() + first + " " + yellow(second) + " " + third);
+    }
+
+    public static void increaseIndent()
+    {
         indent += 1;
     }
 
-    public static void decreaseIndent() {
+    public static void decreaseIndent()
+    {
         indent -= 1;
     }
 
-    public static String getIndent() {
+    public static String getIndent()
+    {
         if( verbose )
             return " " + new String(new char[indent]).replace("\0", "\t");
         else
