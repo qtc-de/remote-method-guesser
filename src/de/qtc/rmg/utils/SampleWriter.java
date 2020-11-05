a13042e5686d67546f59a3f6184e4544dcd75ed7
TNeitzel <qtc_de@outlook.com>
2020-11-05 07:10:08 +0100

    Remove not required imports from SampleWriter

--- src/de/qtc/rmg/utils/SampleWriter.java
+++ src/de/qtc/rmg/utils/SampleWriter.java
@@ -1,8 +1,6 @@
 package de.qtc.rmg.utils;
 
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
