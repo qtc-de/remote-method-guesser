d4806e13331fa73e84073a9ed3e159a4a0cb682f
TNeitzel <qtc_de@outlook.com>
2022-05-06 07:58:14 +0200

    [docker] Improve activation server

--- docker/example-server/resources/server/src/de/qtc/rmg/server/activation/ActivationServer.java
+++ docker/example-server/resources/server/src/de/qtc/rmg/server/activation/ActivationServer.java
@@ -12,17 +12,36 @@ import java.rmi.activation.ActivationGroupDesc;
 import java.rmi.activation.ActivationGroupID;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Properties;
 
+import de.qtc.rmg.server.interfaces.IPlainServer;
+import de.qtc.rmg.server.operations.PlainServer;
 import de.qtc.rmg.server.utils.Logger;
 import de.qtc.rmg.server.utils.Utils;
 
+/**
+ * Create an ActivationServer. This class does basically the same as rmid, but skips some configuration
+ * steps and does only the necessary once. The resulting server seems to work fine, but it is possible
+ * that it is not fully functional due to some missing configuration steps.
+ *
+ * @author Tobias Neitzel (@qtc_de)
+ */
 @SuppressWarnings("unused")
 public class ActivationServer
 {
     private static int activationSystemPort = 1098;
     private static Remote remoteObject1 = null;
+    private static Remote remoteObject2 = null;
+    private static Remote remoteObject3 = null;
 
+    private final static String codebase = "file:///opt/example-server.jar";
+
+    /**
+     * Create the RMI registry and the Activator and bind an ActivationSystem to it. Afterwards, an activation
+     * group is created and two activatable RMI services are bound to the registry. Additionally, we bind one
+     * non activatable service.
+     */
     public static void init()
     {
         Logger.increaseIndent();
@@ -70,10 +89,18 @@ public class ActivationServer
             ActivationGroupID groupID = ActivationGroup.getSystem().registerGroup(groupDesc);
             Utils.toogleOutput();
 
-            ActivationDesc desc = new ActivationDesc(groupID, ActivationService.class.getName(), null, null);
+            ActivationDesc desc = new ActivationDesc(groupID, ActivationService.class.getName(), codebase, null);
             remoteObject1 = Activatable.register(desc);
 
+            ActivationDesc desc2 = new ActivationDesc(groupID, ActivationService2.class.getName(), codebase, null);
+            remoteObject2 = Activatable.register(desc2);
+
+            remoteObject3 = new PlainServer();
+            IPlainServer stub = (IPlainServer)UnicastRemoteObject.exportObject(remoteObject3, 0);
+
             Utils.bindToRegistry(remoteObject1, LocateRegistry.getRegistry(activationSystemPort), "activation-test");
+            Utils.bindToRegistry(remoteObject2, LocateRegistry.getRegistry(activationSystemPort), "activation-test2");
+            Utils.bindToRegistry(stub, LocateRegistry.getRegistry(activationSystemPort), "plain-server");
 
             Logger.println("");
             Logger.decreaseIndent();
