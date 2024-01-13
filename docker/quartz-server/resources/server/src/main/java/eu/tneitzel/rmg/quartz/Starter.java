28e9e306d0b61a80051a9fd462fe97fb7b5b1a12
Tobias Neitzel <tobias.neitzel@code-white.com>
2024-01-13 22:11:14 +0100

    Small formatting changes

--- docker/quartz-server/resources/server/src/main/java/eu/tneitzel/rmg/quartz/Starter.java
+++ docker/quartz-server/resources/server/src/main/java/eu/tneitzel/rmg/quartz/Starter.java
@@ -10,9 +10,9 @@ public class Starter
 {
     public static void main(String[] args)
     {
-    	int jobsExecuted = 0;
-    	Scheduler sched  = null;
-    	
+        int jobsExecuted = 0;
+        Scheduler sched  = null;
+
         try
         {
             System.out.println("[+] Creating a scheduler.");
@@ -22,27 +22,27 @@ public class Starter
 
             System.out.println("[+] Starting scheduler.");
             sched.start();
-            
+
             while (true)
             {
-            	SchedulerMetaData data = sched.getMetaData();
-            	
-            	if (data.getNumberOfJobsExecuted() > jobsExecuted)
-            	{
-            		jobsExecuted = data.getNumberOfJobsExecuted();
-            		System.out.println("[+] Number of executed jobs: " + jobsExecuted);
-            	}
-            	
-            	Thread.sleep(5000);
+                SchedulerMetaData data = sched.getMetaData();
+
+                if (data.getNumberOfJobsExecuted() > jobsExecuted)
+                {
+                    jobsExecuted = data.getNumberOfJobsExecuted();
+                    System.out.println("[+] Number of executed jobs: " + jobsExecuted);
+                }
+
+                Thread.sleep(5000);
             }
         }
-        
+
         catch (SchedulerException e)
         {
             System.out.println("[+] Caught SchedulerException:");
             e.printStackTrace();
         }
-        
+
         catch (InterruptedException e)
         {
             System.out.println("[+] Aborted.:");
@@ -50,18 +50,18 @@ public class Starter
 
         finally
         {
-        	if (sched == null)
-        	{
-        		return;
-        	}
-        	
+            if (sched == null)
+            {
+                return;
+            }
+
             System.out.println("[+] Stopping scheduler.");
-            
+
             try
             {
                 sched.shutdown();
             }
-            
+
             catch (SchedulerException e)
             {
                 System.out.println("[+] Caught SchedulerException:");
